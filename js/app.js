f80b00f0e292896f2d58f18f509abe95b2d7050d
Cody Alejo <cody.alejo1997@gmail.com>
2021-05-25 17:37:34 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+baguetteBox.run('.gallery', {
+    captions: true, // display image captions.
+    buttons: 'auto', // arrows navigation
+    fullScreen: false,
+    noScrollbars: false,
+    bodyClass: 'baguetteBox-open',
+    titleTag: false,
+    async: false,
+    preload: 2,
+    animation: 'slideIn', // fadeIn or slideIn
+    verlayBackgroundColor: 'rgba(0,0,0,.8)'
+  });
