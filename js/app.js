061bbdcbe9ee8a43827fc08b212e5073194af1b2
Cody Alejo <cody.alejo1997@gmail.com>
2021-05-27 14:58:30 -0500

    updated app.js
    created a light show effect and search filter in app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,2 @@
-baguetteBox.run('.gallery', {
-    captions: true, // display image captions.
-    buttons: 'auto', // arrows navigation
-    fullScreen: false,
-    noScrollbars: false,
-    bodyClass: 'baguetteBox-open',
-    titleTag: false,
-    async: false,
-    preload: 2,
-    animation: 'slideIn', // fadeIn or slideIn
-    verlayBackgroundColor: 'rgba(0,0,0,.8)'
-  });
+baguetteBox.run('.gallery');
+const search = new Filter("search", "data-caption");
